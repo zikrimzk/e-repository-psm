bff708a1ccbf0447c5f451e91ec808bdb7c32f5f
MUHAMMAD ZIKRI BIN KASHIM <zikrimzk@gmail.com>
2025-02-25 21:20:10 +0800

    update main

--- resources/views/staff/layouts/main.blade.php
+++ resources/views/staff/layouts/main.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr" data-nav-layout="vertical" data-theme-mode="light" data-header-styles="light"
-    data-menu-styles="color" data-toggled="close" data-nav-style="menu-hover" data-width="boxed" loader="enable">
+    data-menu-styles="color" data-toggled="close" data-nav-style="menu-hover">
 
 <head>
 
